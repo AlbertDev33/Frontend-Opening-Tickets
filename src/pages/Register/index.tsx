47e07296af981a840fc797dbd07630d5abb9470d
AlbertDev33 <alber725@gmail.com>
2020-10-22 20:25:52 -0300

    Importação da interface FormHandles para recuperar a função setErrors. Importação da função getValidationErrors

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+
+import getValidationErrors from '../../utils/getValidationErros';
 
 import { Container, Content, Background } from './styles';
 
@@ -10,8 +13,12 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const Register: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -24,7 +31,8 @@ const Register: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -32,7 +40,7 @@ const Register: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
