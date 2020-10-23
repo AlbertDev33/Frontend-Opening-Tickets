973f0a5cdaebeb4a055a6782adb57878ec271532
AlbertDev33 <alber725@gmail.com>
2020-10-23 10:23:03 -0300

    Troca do componente Route do React pelo Route que eu criei. Passei a propriedade isPrivate para identificar se é uma rota que precisa ser acessada por um usuário logado no app.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
-    <Route path="/register" exact component={Register} />
+    <Route path="/register" component={Register} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
