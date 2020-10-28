d95325456c749f543f1078bcaac803bc6072af2e
AlbertDev33 <alber725@gmail.com>
2020-10-28 15:30:58 -0300

    Estilização do componente textarea e do Error incluída

--- src/components/TextArea/styles.ts
+++ src/components/TextArea/styles.ts
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
@@ -6,6 +8,59 @@ interface ContainerProps {
   isErrored: boolean;
 }
 
-export const Container = styled.div<ContainerProps>``;
+export const Container = styled.div<ContainerProps>`
+  display: flex;
 
-export const Error = styled.div``;
+  textarea {
+    height: 170px;
+    width: 100%;
+    background: #ecf0f1;
+    border-radius: 10px;
+    padding: 16px;
+    margin-top: 10px;
+    resize: none;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+
+    border: 3px solid #ecf0f1;
+    color: #666360;
+
+    display: flex;
+    align-items: center;
+
+    ${props =>
+      props.isErrored &&
+      css`
+        border-color: #c53030;
+      `}
+
+    ${props =>
+      props.isFocused &&
+      css`
+        color: #666360;
+        border-color: #6ec4db;
+      `}
+  }
+
+  div {
+    position: relative;
+    svg {
+      position: absolute;
+      right: 30px;
+      top: 20px;
+    }
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+  }
+`;
