e42efbfec085eeb9b0796c36f693bea9a5515a8b
AlbertDev33 <alber725@gmail.com>
2020-10-28 11:10:39 -0300

    Criação do componente TextArea

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,67 @@
+import React, {
+  InputHTMLAttributes,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useField } from '@unform/core';
+
+import { Container, Error } from './styles';
+
+// Sobrescrevendo a propriedade name do input, tornando-a obrigatória
+interface InputProps extends InputHTMLAttributes<HTMLTextAreaElement> {
+  name: string;
+  // Tornando o ícone como um componente do React
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const TextArea: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const { fieldName, error, defaultValue, registerField } = useField(name);
+
+  const handleInputfocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    inputRef.current?.value ? setIsFilled(true) : setIsFilled(false);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
+      {Icon && <Icon size={20} />}
+      <textarea
+        onFocus={handleInputfocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
+    </Container>
+  );
+};
+
+export default TextArea;
