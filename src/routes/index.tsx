e3d26250192cb2fb62307a1f275ead46074f9398
AlbertDev33 <alber725@gmail.com>
2020-10-28 11:02:32 -0300

    Acrescentada a rota para criar um ticket

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import Route from './Route';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Dashboard from '../pages/Dashboard';
+import Tickets from '../pages/Tickets';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -13,6 +14,7 @@ const Routes: React.FC = () => (
     <Route path="/register" component={Register} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/tickets" component={Tickets} isPrivate />
   </Switch>
 );
 
