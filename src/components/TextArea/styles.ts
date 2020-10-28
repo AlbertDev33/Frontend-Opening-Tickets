9c79636c0dc11889d913fb969cbfa79f61340eed
AlbertDev33 <alber725@gmail.com>
2020-10-28 11:09:32 -0300

    Estilo inicial do componente TextArea

---
+++ src/components/TextArea/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>``;
+
+export const Error = styled.div``;
