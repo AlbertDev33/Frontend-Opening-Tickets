22d678dc5431a1a289c5a55dd6197ac4173c8978
AlbertDev33 <alber725@gmail.com>
2021-02-02 21:34:09 -0300

    feat: add style component

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: auto;
+  margin-left: 16rem;
+  position: relative;
+  padding: 0 4rem;
+`;
+
+export const OpenTicketsText = styled.span`
+  display: flex;
+  align-items: center;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 600;
+  color: #212121;
+
+  svg {
+    margin-right: 1rem;
+    color: #aaa5a5;
+  }
+`;
